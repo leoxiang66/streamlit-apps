6f3e6c496d876c24d8355022ee7e31b93002663e
Tao Xiang <xiang.tao@outlook.de>
2022-10-29 11:38:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,60 @@ from requests_toolkit import  AirQualityQuery
 
 
 st.markdown('''# Country Air Quality Ranking''')
-country =  st.text_input(
-    label=' ',
-    placeholder=' Which country do you want to query?',
-    # label_visibility = 'collapsed'
-)
+with st.form(key='my form'):
+    country = st.text_input(
+        label='Country:',
+        placeholder=' Which country do you want to query?',
+        # label_visibility = 'collapsed'
+    )
 
-if country is not None and country != '':
-    md_head = '''| ID | City |Province | US AQI |
+    # include_province = st.checkbox('search specific province')
+    province = st.text_input(
+        label='Province:',
+        placeholder= f'''Which province do you want to query? Leave blank for all provinces''',
+    )
+
+    submit = st.form_submit_button('Search')
+
+# print(dict(
+#     country = country,
+#     province = province,
+# ))
+
+st.markdown(f'''You entered:
+Country: `{country}`, 
+Province:`{province}`
+''')
+
+
+if country !='':
+    # all provinces
+    if province =='':
+        md_head = '''| ID | City |Province | US AQI |
 | -------- | -------- | -------- | -------- |
 '''
-    md= st.empty()
-    generator = AirQualityQuery.air_quality_by_country(country, return_frequency=10)
-    while True:
-        try:
-            with st.spinner():
-                i = next(generator)
-            new_md = ''
-            for id, j in enumerate(i):
-                new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
-            md.markdown(md_head + new_md)
-        except StopIteration:
-            break
-
-
+        md = st.empty()
+        generator = AirQualityQuery.air_quality_by_country(country, return_frequency=10)
+        while True:
+            try:
+                with st.spinner():
+                    i = next(generator)
+                new_md = ''
+                for id, j in enumerate(i):
+                    new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
+                md.markdown(md_head + new_md)
+            except StopIteration:
+                break
+    # specific province
+    else:
+        md_head = '''| ID | City |Province | US AQI |
+| -------- | -------- | -------- | -------- |
+'''
+        md = st.empty()
+        i = AirQualityQuery.air_quality_by_province_country(country, province)
+        new_md = ''
+        for id, j in enumerate(i):
+            new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
+        md.markdown(md_head + new_md)
 
 
