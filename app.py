4c86179af9afecabf69ce6dabf92bb95b759f489
Tao Xiang <xiang.tao@outlook.de>
2022-10-29 11:42:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,40 +23,42 @@ with st.form(key='my form'):
 #     province = province,
 # ))
 
-st.markdown(f'''You entered:
-Country: `{country}`, 
-Province:`{province}`
-''')
+if submit:
+    st.markdown(f'''You entered:
+    Country: `{country}`, 
+    Province:`{province}`
+    ''')
 
-
-if country !='':
-    # all provinces
-    if province =='':
-        md_head = '''| ID | City |Province | US AQI |
+    if country != '':
+        # all provinces
+        if province == '':
+            md_head = '''| ID | City |Province | US AQI |
 | -------- | -------- | -------- | -------- |
 '''
-        md = st.empty()
-        generator = AirQualityQuery.air_quality_by_country(country, return_frequency=10)
-        while True:
-            try:
-                with st.spinner():
-                    i = next(generator)
-                new_md = ''
-                for id, j in enumerate(i):
-                    new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
-                md.markdown(md_head + new_md)
-            except StopIteration:
-                break
-    # specific province
-    else:
-        md_head = '''| ID | City |Province | US AQI |
+            md = st.empty()
+            generator = AirQualityQuery.air_quality_by_country(country, return_frequency=10)
+            while True:
+                try:
+                    with st.spinner():
+                        i = next(generator)
+                    new_md = ''
+                    for id, j in enumerate(i):
+                        new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
+                    md.markdown(md_head + new_md)
+                except StopIteration:
+                    break
+        # specific province
+        else:
+            md_head = '''| ID | City |Province | US AQI |
 | -------- | -------- | -------- | -------- |
 '''
-        md = st.empty()
-        i = AirQualityQuery.air_quality_by_province_country(country, province)
-        new_md = ''
-        for id, j in enumerate(i):
-            new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
-        md.markdown(md_head + new_md)
+            md = st.empty()
+            i = AirQualityQuery.air_quality_by_province_country(country, province)
+            new_md = ''
+            for id, j in enumerate(i):
+                new_md += f'''|{id + 1}|{j[0]}|{j[1]}|{j[2]}|\n'''
+            md.markdown(md_head + new_md)
+
+
 
 
