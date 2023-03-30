b1facaa5bf2bb8bc152ffa24414344ee4642c694
Tao Xiang <xiang.tao@outlook.de>
2023-03-30 10:56:44 +0200

    1

---
+++ test.py
@@ -0,0 +1,25 @@
+import sthelper as sth
+import streamlit as st
+
+
+def homepage():
+    st.markdown("# Homepage")
+    st.button('to test', on_click=lambda : session.go_to_page('test'))
+
+def testpage():
+    st.markdown("# Test page")
+
+
+
+session = sth.OpenSession(dict(
+    home = homepage,
+    test = testpage
+),
+    current_page='home'
+)
+
+session.render()
+
+print(session.get_current_page())
+
+
