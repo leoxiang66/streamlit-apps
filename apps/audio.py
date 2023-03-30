250d5ab1c5eb5650d8144a9500d66c513dbad9d3
Tao Xiang <xiang.tao@outlook.de>
2023-03-30 15:41:20 +0200

    1

--- apps/audio.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-audio_file = open('./apps/tishi.mp3', 'rb')
-audio_bytes = audio_file.read()
-
-audio = st.audio(audio_bytes, format='audio/ogg')
--- apps/audio_play.py
+++
@@ -1,4 +0,0 @@
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("./apps/tishi.mp3")
-pygame.mixer.music.play()
