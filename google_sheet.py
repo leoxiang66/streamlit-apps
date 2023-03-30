fdeff8723d77267e44269e5273851b34cf75e83d
Tao Xiang <xiang.tao@outlook.de>
2023-03-30 15:12:07 +0200

    1

--- google_sheet.py
+++ google_sheet.py
@@ -1,30 +1,28 @@
 # streamlit_app.py
 import streamlit as st
-from google.oauth2 import service_account
-from gsheetsdb import connect
+from shillelagh.backends.apsw.db import connect
 
-# Create a connection object.
-conn = connect(credentials=(service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)))
+# # Create a connection object.
+# conn = connect(credentials=(service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"],
+#     scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#     ],
+# )))
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 # @st.cache_data(ttl=600)
 def run_query(query):
-    cursor = login_to_google()
+    cursor = __login_to_google__()
     sheet_url = st.secrets["private_gsheets_url"]
-    query = query.replace('SHEET',sheet_url)
-    print(query)
+    query = query.replace('SHEET',f'''"{sheet_url}"''')
     dataset = cursor.execute(query)
     return dataset
 
 
 
-def login_to_google():
+def __login_to_google__():
     print("\n\n##########################\n\n")
     print("Login to Google API")
 
@@ -52,16 +50,12 @@ def login_to_google():
 #                       #
 #########################
 
-# def get_whole_dataset():
-#     cursor = login_to_google()
-#     query = f'SELECT * FROM "{APIs["essay_gsheets_url"]}"'
-#     dataset = cursor.execute(query)
-#     return dataset
+def get_whole_dataset():
+    query = f'SELECT * FROM SHEET'
+    dataset = run_query(query)
+    return dataset
 
-# rows = run_query(f'''SELECT * FROM SHEET''')
+rows = run_query(f'''SELECT * FROM SHEET''')
 
-# Print results.
-# for row in rows:
-#     st.write(f"{row.name} has a :{row.pet}:")
 
-login_to_google()
+print(list(rows))
